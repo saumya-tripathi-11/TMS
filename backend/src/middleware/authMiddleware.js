3592f729eb43e10c2268b6b6f7d79aff0621bffc
Saumya Tripathi <tripathi.saumya2004@gmail.com>
2026-07-17 23:25:21 +0530

    Update authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const protect = async (req, res, next) => {
   let token;
 
-  // Read token from Authorization: Bearer <token>
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer ')
